443db61b36b2817a9f1ded007a5f9e083fd5ff2c
Alex Artemov <artemov.sandro@gmail.com>
2023-10-16 16:49:53 -0400

    created a firestore database

---
+++ src/connectDb.js
@@ -0,0 +1,12 @@
+// import the tools we need from our libraries
+import { initializeApp, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+// import our secret credentials
+import { creds } from "./creds.js";
+
+// connect to our firebase project with our credentials
+initializeApp({
+    credential: cert(creds),
+})
+// once connected, connect to Firestore database
+export const db = getFirestore()
